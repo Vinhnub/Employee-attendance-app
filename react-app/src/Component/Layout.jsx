f80413af3bbb59523feb76deb566e0881592d6ff
Vinh Nub <vinhnguyenvan1255@gmail.com>
2025-11-24 15:09:18 +0700

    None

--- react-app/src/Component/Layout.jsx
+++ react-app/src/Component/Layout.jsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import Navbar from './Navbar';
-import { Outlet } from 'react-router-dom';
+import React from "react";
 
-const Layout = () => {
+export default function Layout({Header,Navbar, children}) {
   return (
-    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <Navbar />
-      <div style={{ flex: 1, padding: '20px' }}>
-        <Outlet /> {/* Nơi nội dung các trang sẽ hiển thị */}
-      </div>
-      <footer style={{ textAlign: 'center', padding: '20px', color: '#888', fontSize: '0.8rem' }}>
-        © 2025 Coffee Shop System. Designed for Baristas.
-      </footer>
+    <div>
+      <Header/>
+      <Navbar/>
+      {children}
     </div>
-  );
-};
-
-export default Layout;
+  )
+}
