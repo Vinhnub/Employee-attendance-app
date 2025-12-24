59d237ebc2540e817c6fcea59a9eb460a7f6112d
quan23 <xuanwc321@gmail.com>
2025-12-24 19:34:32 +0700

    .

--- react-app/src/Page/WorkPage.jsx
+++ react-app/src/Page/WorkPage.jsx
@@ -10,6 +10,22 @@ export default function WorkPage() {
   const [shifts, setShifts] = useState([]);
   const [expandedShift, setExpandedShift] = useState(null);
   const { popup } = usePopup();
+  const [user, setUser] = useState();
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await authService.me();
+        if (response.data.status === "success") {
+          setCurrentUser(response.data.data);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchCurrentUser();
+  }, []);
   useEffect(() => {
     const fetchShifts = async () => {
       try {
@@ -31,6 +47,7 @@ export default function WorkPage() {
       <h2 className={styles.title}>Ca làm việc trong tháng</h2>
       <ShiftsTable
         shifts={shifts}
+        user={currentUser}
         allowExpand={true}
         expandedShift={expandedShift}
         setExpandedShift={setExpandedShift}
